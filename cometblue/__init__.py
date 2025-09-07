c3cbf400f81a68b355f261c7693c9ab5a0a4b4e9
Richard <42204099+rikroe@users.noreply.github.com>
2025-09-07 13:37:48 +0200

    Get server pin

--- cometblue/__init__.py
+++ cometblue/__init__.py
@@ -71,7 +71,7 @@ class CometBlueBleakClient(BleakClient):
 
     def __init__(self, *args, **kwargs):
         _LOGGER.debug("Initializing client: %s, %s", args, kwargs)
-        self.server_pin = kwargs.pop("server_pin")
+        self.server_pin = kwargs.get("server_pin")
         _LOGGER.debug("Pin init: %s", self.server_pin)
         super().__init__(*args, **kwargs)
 
